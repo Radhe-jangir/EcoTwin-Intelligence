78c07d7ef6522db92a2b56c82e3ca1ed455eb534
Radhe-jangir <jangirradhe175@gmail.com>
2026-06-21 21:39:23 +0530

    Remove bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,14 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(() => {
   return {
     plugins: [
       react(),
       tailwindcss(),
-      visualizer({
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-        filename: 'dist/stats.html'
-      })
+     
     ],
     resolve: {
       alias: {
